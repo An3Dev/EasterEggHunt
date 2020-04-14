76e2d4670de20cf76c90a9b58742b24ea5f93df4
An3Dev <an3Developer@gmail.com>
2020-04-13 17:52:22 -0700

    Initial commit

---
+++ Assets/Scripts/EggSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggSpawner : MonoBehaviour
+{
+
+    public GameObject goldenEggPrefab, eggPrefab, egg2Prefab;
+
+    public int numOfEggs;
+
+    public int range = 60;
+
+    public LayerMask mask;
+
+    public static EggSpawner Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        for (int i = 0; i < numOfEggs; i++)
+        {
+            int random = Random.Range(0, 1);
+            GameObject egg = null;
+
+            Vector3 randomPosition;
+            randomPosition = new Vector3(transform.position.x + Random.Range(-range, range), 30, transform.position.z + Random.Range(-range, range));
+
+            if (random == 1)
+            {
+                egg = Instantiate(eggPrefab, randomPosition, Quaternion.identity);
+            } else
+            {
+                egg = Instantiate(egg2Prefab, randomPosition, Quaternion.identity);
+            }
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(egg.transform.position, Vector3.down, out hit, mask))
+            {
+                egg.transform.position = hit.point + Vector3.up * 0.2f;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/PickUpEggs.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PickUpEggs : MonoBehaviour
+{
+
+    bool nearEgg = false;
+
+    Camera mainCamera;
+
+    public LayerMask eggMask;
+
+    public TextMeshProUGUI eggText;
+
+    int eggsCollected;
+
+    int eggsSpawned = 0;
+
+    public GameObject confettiPrefab;
+
+    public TextMeshProUGUI timerText;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GetComponentInChildren<Camera>();
+        eggsSpawned = EggSpawner.Instance.numOfEggs;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = Mathf.Floor(timer % 60).ToString("00");
+
+       
+        timerText.text = minutes + ":" + seconds;
+
+        if (eggsCollected >= eggsSpawned)
+        {
+            // Won game
+
+        }
+
+        //if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        //{
+            if (Input.GetMouseButton(0) && nearEgg)
+            {
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, 10, eggMask))
+                {
+                    hit.collider.gameObject.SetActive(false);
+
+                    AddEgg(hit.point, hit.collider.transform);
+                }
+            }
+        //} else if (Application.platform == RuntimePlatform.Android)
+        //{
+        //    if (Input.touchCount > 0 && nearEgg)
+        //    {
+        //        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        //        {
+        //            Vector2 touchPos = Input.GetTouch(0).position;
+
+        //            Ray ray = mainCamera.ScreenPointToRay(touchPos);
+
+        //            RaycastHit hit;
+
+        //            if (Physics.Raycast(ray, out hit, 15, eggMask))
+        //            {
+        //                hit.collider.gameObject.SetActive(false);
+
+        //                AddEgg(hit.point, hit.collider.transform);
+        //            }
+        //        }
+                
+        //    }
+        //}
+    }
+
+    void AddEgg(Vector3 position, Transform egg)
+    {
+
+        if (egg.CompareTag("GoldenEgg"))
+        {
+            eggsCollected += 5;
+        } else
+        {
+            eggsCollected++;
+        }
+
+        eggText.text = eggsCollected.ToString();
+
+        // spawn confetti effect
+        GameObject confetti = Instantiate(confettiPrefab, egg.position, Quaternion.identity);
+        Destroy(confetti, 31);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Egg"))
+        {
+            nearEgg = true;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Egg"))
+        {
+            nearEgg = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Egg"))
+        {
+            nearEgg = false;
+        }
+    }
+}
+
+
